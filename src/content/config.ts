4bc2547361c10e76335eb98dfe7d7917105c9f20
Radoth <58544593+radoth@users.noreply.github.com>
2024-06-20 20:23:26 +0800

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,84 +1,103 @@
----
-import { getCollection } from "astro:content";
-import { getImage } from 'astro:assets';
-import Homepage from "../components/Homepage.astro";
+// 1. Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
 
-const collections = ['long_form', 'short_form', 'muses', 'zeitweilig'];
+// 2. Define a `type` and `schema` for each collection
+const muses = defineCollection({
+  type: "content", // v2.5.0 and later
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    author: z.string(),
+    description: z.string(),
+    image: z.object({
+      src: z.string(),
+      alt: z.string(),
+      positionx: z.string().optional(),
+      positiony: z.string().optional()
+    }).optional(),
+    subimage: z.array(z.object({
+      src: z.string(),
+      alt: z.string(),
+      positionx: z.string().optional(),
+      positiony: z.string().optional()
+    })).optional(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+  }),
+});
 
-async function getRandomImages() {
-  let allImages = [];
-  let allAlts = [];
-  let allUrls = [];
+const short_form = defineCollection({
+  type: "content", // v2.5.0 and later
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    author: z.string(),
+    description: z.string(),
+    image: z.object({
+      src: z.string(),
+      alt: z.string(),
+      positionx: z.string().optional(),
+      positiony: z.string().optional()
+    }).optional(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+  }),
+});
 
-  for (let collection of collections) {
-    const allContent = await getCollection(collection);
+const long_form = defineCollection({
+  type: "content", // v2.5.0 and later
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    author: z.string(),
+    description: z.string(),
+    image: z.object({
+      src: z.string(),
+      alt: z.string(),
+      positionx: z.string().optional(),
+      positiony: z.string().optional()
+    }).optional(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+  }),
+});
 
-    // Filter posts with images and subimages
-    const postsWithImages = allContent.filter(post =>
-      (post.data.image?.src && post.data.image?.alt) ||
-      (post.data.subimage && post.data.subimage.length > 0)
-    );
+const zeitweilig = defineCollection({
+  type: "content", // v2.5.0 and later
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    author: z.string(),
+    description: z.string(),
+    image: z.object({
+      src: z.string(),
+      alt: z.string(),
+      positionx: z.string().optional(),
+      positiony: z.string().optional()
+    }).optional(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+  }),
+});
 
-    const imagePromises = postsWithImages.map(async post => {
-      let images = [];
+const authors = defineCollection({
+  type: "content", // v2.5.0 and later
+  schema: z.object({
+    title: z.string(),
+    tags: z.array(z.string()),
+    author: z.string(),
+    description: z.string(),
+    image: z.object({
+      src: z.string(),
+      alt: z.string(),
+      positionx: z.string().optional(),
+      positiony: z.string().optional()
+    }).optional(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+  }),
+});
 
-      // Add main image if exists
-      if (post.data.image?.src && post.data.image?.alt) {
-        images.push({
-          src: post.data.image.src,
-          alt: post.data.image.alt,
-          positionx: post.data.image.positionx,
-          positiony: post.data.image.positiony
-        });
-      }
 
-      // Add subimages if exist
-      if (post.data.subimage && post.data.subimage.length > 0) {
-        images.push(...post.data.subimage);
-      }
-
-      // Randomly select an image from the images array
-      const randomImage = images[Math.floor(Math.random() * images.length)];
-
-      const imageAsset = await getImage({
-        src: randomImage.src,
-        alt: randomImage.alt,
-        width: "1920",
-        height: "1080",
-        decoding: "async",
-        format: "avif",
-        quality: "65"
-      });
-
-      return {
-        imageSrc: imageAsset ? imageAsset.src : null,
-        alt: randomImage.alt,
-        url: `/${collection}/${post.slug}`
-      };
-    });
-
-    const results = await Promise.all(imagePromises);
-
-    allImages.push(...results.map(result => result.imageSrc));
-    allAlts.push(...results.map(result => result.alt));
-    allUrls.push(...results.map(result => result.url));
-  }
-
-  return { allImages, allAlts, allUrls };
-}
-
-const { allImages, allAlts, allUrls } = await getRandomImages();
----
-
-{allImages.length > 0 ? (
-  <Homepage 
-    title={Astro.props.pageTitle} 
-    images={JSON.stringify(allImages)} 
-    alt={JSON.stringify(allAlts)} 
-    urls={JSON.stringify(allUrls)} 
-    width="1920" 
-    height="1080" 
-  />
-) : (
-  <p>No images found</p>
-)}
+// 3. Export a single `collections` object to register your collection(s)
+export const collections = { muses, short_form, long_form, zeitweilig, authors };
